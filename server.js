4eec2b25c8b5860c07b9149311c7b3ddad8a1357
Subhana Saman <subhanariaz12@gmail.com>
2026-05-04 15:35:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(cors({
   // Multiple origins allowed (ENV variable + Hardcoded for safety)
   origin: [
     process.env.FRONTEND_URL, 
-    'http://localhost:5173/'
+    'http://localhost:5173'
   ],
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
