9af405fa3eaa15668953715d711036cae1df56e4
BeenaAnum <beenaanum@gmail.com>
2026-05-04 11:33:22 +0500

    backend code

---
+++ middlerware/protect.js
@@ -0,0 +1,15 @@
+//This is the 'bouncer' — it checks the JWT token before allowing access to protected routes.
+
+import jwt from 'jsonwebtoken';
+export const protect = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token)
+    return res.status(401).json({ msg: 'Not authorized, no token' });
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    req.userId = decoded.userId;
+        next();
+  } catch {
+    res.status(401).json({ msg: 'Token is invalid' });
+  }
+};
---
+++ server.js
@@ -0,0 +1,21 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+import connectDB from './config/db.js';
+import authRoutes from './routes/auth.js';
+const app = express();
+console.log("MONGO_URI:", process.env.MONGO_URI);
+connectDB();
+app.use(cors({
+  origin: process.env.FRONTEND_URL,
+  credentials: true
+}));
+app.use(express.json());
+app.use(cookieParser());
+app.use('/api/auth', authRoutes);
+app.get('/', (req, res) => res.send('API is running...'));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+
+console.log("MONGO_URI:", process.env.MONGO_URI);
